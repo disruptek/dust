72ae754fd6df7997ad9ba4fedef8ce3330bed076
Andy Davidoff <andy+github@disruptek.com>
2020-08-17 14:27:25 -0400

    add hashing for arbitrary nodes

---
+++ dust/hashing.nim
@@ -0,0 +1,13 @@
+import std/sequtils
+import std/sets
+
+include compiler/sighashes # because too few things are exported
+
+const
+  considerAll = {ConsiderFlag.low .. ConsiderFlag.high}
+
+proc hashNode*(n: PNode; flags: set[ConsiderFlag] = considerAll): SigHash =
+  var c: MD5Context
+  md5Init c
+  hashTree(c, n, flags)
+  md5Final(c, result.MD5Digest)
